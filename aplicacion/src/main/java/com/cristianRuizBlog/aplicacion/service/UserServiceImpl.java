6056cfb969370b27e56c0b489d8c72bcf9d71cda
manteniendowordpress <info@manteniendowordpress.com>
2021-01-21 22:25:22 -0300

    Implement Update User Service. Colse #6

--- aplicacion/src/main/java/com/cristianRuizBlog/aplicacion/service/UserServiceImpl.java
+++ aplicacion/src/main/java/com/cristianRuizBlog/aplicacion/service/UserServiceImpl.java
@@ -30,6 +30,9 @@ public class UserServiceImpl implements UserService {
 	}
 	
 	private boolean checkPasswordValid(User user) throws Exception {
+		if(user.getConfirmPassword() == null || user.getConfirmPassword().isEmpty()) {
+			throw new Exception("Confirm Password es obligatorio");
+		}
 		if(!user.getPassword().equals(user.getConfirmPassword())) {
 			throw new Exception("Password y Confirm Password no son iguales");
 		}
@@ -43,6 +46,33 @@ public class UserServiceImpl implements UserService {
 			user = userRepository.save(user);
 		}
 		return user;
+	}
+
+
+	@Override
+	public User getUserById(Long id) throws Exception {
+		return userRepository.findById(id).orElseThrow(() -> new Exception("El usuario para editar no existe"));
+	}
+
+
+	@Override
+	public User updateUser(User fromUser) throws Exception {
+		User toUser = getUserById(fromUser.getId());
+		mapUser(fromUser, toUser);
+		return userRepository.save(toUser);
+	}
+	
+	/**
+	 * Map everything but the password.
+	 * @param from
+	 * @param to
+	 */
+	protected void mapUser(User from, User to) {
+		to.setUsername(from.getUsername());
+		to.setFirstName(from.getFirstName());
+		to.setLastName(from.getLastName());
+		to.setEmail(from.getEmail());
+		to.setRoles(from.getRoles());
 	}
 
 }
